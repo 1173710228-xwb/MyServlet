5c16dc6bd9f5312211b8301ee4ab73162a2bc502
1173710228-xwb <1762526288@qq.com>
2020-09-09 19:11:42 +0800

    update: 增加了servlet测试类

--- src/main/java/com/xwb/servlets/HelloServlet.java
+++ src/main/java/com/xwb/servlets/HelloServlet.java
@@ -1,14 +1,16 @@
 package com.xwb.servlets;
 
+import com.sun.corba.se.spi.activation.Server;
+
 import javax.servlet.*;
 import java.io.IOException;
 
 /**
- * @ClassName : TestServlet
+ * @ClassName : HelloServlet
  * @Author : 许文滨
- * @Date: 2020-09-09 11:11
+ * @Date: 2020-09-09 19:09
  */
-public class TestServlet implements Servlet {
+public class HelloServlet implements Servlet {
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
 
@@ -21,7 +23,7 @@ public class TestServlet implements Servlet {
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        servletResponse.getWriter().println("Hello TestServlet!");
+        servletResponse.getWriter().println("Hello HelloServlet!");
     }
 
     @Override
